16bc45390216a838afb74eeaf428ae79a0be40b2
Nikola Pavkovic <nikola.pavkovic@infobip.com>
2023-08-26 14:15:42 +0200

    Added lowercase path support, enabled locator

---
+++ src/test/java/com/properbooker/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.properbooker.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
